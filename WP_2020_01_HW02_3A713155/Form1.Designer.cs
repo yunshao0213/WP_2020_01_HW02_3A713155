bfd6cb1e0663c84e987768dfd82a34ee4e4832d7
Until Forever <Until Forever@UNTILFOREVER-PC>
2020-11-05 23:18:14 +0800

    加入專案檔案。

---
+++ WP_2020_01_HW02_3A713155/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace WP_2020_01_HW02_3A713155
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pic = new System.Windows.Forms.PictureBox();
+            this.btn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pic
+            // 
+            this.pic.Location = new System.Drawing.Point(95, 47);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(271, 362);
+            this.pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic.TabIndex = 0;
+            this.pic.TabStop = false;
+            // 
+            // btn
+            // 
+            this.btn.Font = new System.Drawing.Font("新細明體", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn.Location = new System.Drawing.Point(95, 442);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(270, 81);
+            this.btn.TabIndex = 1;
+            this.btn.Text = "抽牌";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(472, 576);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.pic);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pic;
+        private System.Windows.Forms.Button btn;
+    }
+}
+
---
+++ WP_2020_01_HW02_3A713155/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WP_2020_01_HW02_3A713155.Properties;
+
+namespace WP_2020_01_HW02_3A713155
+{
+    public partial class Form1 : Form
+    {
+        List<Image> result = new List<Image>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            result.Add(Resources._1);
+            result.Add(Resources._2);
+            result.Add(Resources._3);
+            result.Add(Resources._4);
+            result.Add(Resources._5);
+            result.Add(Resources._6);
+            result.Add(Resources._7);
+            result.Add(Resources._8);
+            result.Add(Resources._9);
+            result.Add(Resources._10);
+            result.Add(Resources._11);
+            result.Add(Resources._12);
+            result.Add(Resources._13);
+            result.Add(Resources._14);
+            result.Add(Resources._15);
+            result.Add(Resources._16);
+            result.Add(Resources._17);
+            result.Add(Resources._18);
+            result.Add(Resources._19);
+            result.Add(Resources._20);
+            result.Add(Resources._21);
+            result.Add(Resources._22);
+            result.Add(Resources._23);
+            result.Add(Resources._24);
+            result.Add(Resources._25);
+            result.Add(Resources._26);
+            result.Add(Resources._27);
+            result.Add(Resources._28);
+            result.Add(Resources._29);
+            result.Add(Resources._30);
+            result.Add(Resources._31);
+            result.Add(Resources._32);
+            result.Add(Resources._33);
+            result.Add(Resources._34);
+            result.Add(Resources._35);
+            result.Add(Resources._36);
+            result.Add(Resources._37);
+            result.Add(Resources._38);
+            result.Add(Resources._39);
+            result.Add(Resources._40);
+            result.Add(Resources._41);
+            result.Add(Resources._42);
+            result.Add(Resources._43);
+            result.Add(Resources._44);
+            result.Add(Resources._45);
+            result.Add(Resources._46);
+            result.Add(Resources._47);
+            result.Add(Resources._48);
+            result.Add(Resources._49);
+            result.Add(Resources._50);
+            result.Add(Resources._51);
+            result.Add(Resources._52);
+            
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            int rindex = (new Random()).Next(51) + 1; 
+            pic.Image = result[rindex];
+        }
+    }
+}
